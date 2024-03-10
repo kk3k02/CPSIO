8f8c7a73dd1941bfdc5637586d03c9bbb98d47ab
kk3k02 <kubajak@outlook.com>
2024-03-10 15:23:40 +0100

    Division into classes

--- main.py
+++ main.py
@@ -1,46 +1,5 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-def load_signal(file_path, frequency):
-    """
-    Funkcja do wczytywania sygnału EKG z pliku tekstowego.
-    Założenie: Jeśli pierwsza kolumna zawiera czas, to jest używana do skalowania osi czasu.
-    """
-
-    data = np.genfromtxt(file_path)
-    if data.ndim == 2:  # File has 2 dimensions
-        if data.shape[1] > 2:  # File has more than 1 column
-            time = np.arange(len(data)) / frequency
-            signal = data
-        else:  # File has only 1 column
-            time = data[:, 0]
-            signal = data[:, 1]
-    else:  # File has only 1 dimension
-        time = np.arange(len(data)) / frequency
-        signal = data
-
-    return time, signal
-
-
-def plot_signal(time, signal, start_time, end_time, title="Sygnał EKG", xlabel="Czas (s)", ylabel="Amplituda",
-                save_path=None):
-    """
-    Funkcja do wizualizacji sygnału EKG.
-    """
-    plt.figure(figsize=(10, 6))
-    plt.plot(time, signal, label="Sygnał EKG")
-    plt.xlim(start_time, end_time)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend()
-    plt.grid(True)
-
-    if save_path:
-        plt.savefig(save_path)
-    else:
-        plt.show()
+from readFile import File
+from generatePlot import plot
 
 
 def main():
@@ -50,8 +9,10 @@ def main():
     end_time = float(input("Podaj końcowy czas wyświetlanego wycinka [s]: "))
     save_path = input("Podaj ścieżkę do zapisu wycinka (jeśli nie chcesz zapisać, pozostaw puste): ")
 
-    time, signal = load_signal(file_path, frequency)
-    plot_signal(time, signal, start_time, end_time, save_path=save_path)
+    file = File(file_path, frequency)
+    time, signal = file.load_EKG()
+    plot_signal = plot(time, signal, start_time, end_time, "Sygnał EKG", "Czas [s]", "Amplituda", None)
+    plot_signal.generate_Plot()
 
 
 if __name__ == "__main__":
