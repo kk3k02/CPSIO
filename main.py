243927e4bd089a6d4198e6d7b9e952b972c0dcb5
kk3k02 <kubajak@outlook.com>
2024-03-10 13:54:58 +0100

    Fixed: read EKG from txt + show one of the selected signal

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def load_signal(file_path):
+    """
+    Funkcja do wczytywania sygnału EKG z pliku tekstowego.
+    Założenie: Jeśli pierwsza kolumna zawiera czas, to jest używana do skalowania osi czasu.
+    """
+
+    data = np.loadtxt(file_path)
+
+    if data.shape[1] == 2:  # Jeśli są dwie kolumny, to pierwsza to czas, a druga to sygnał EKG
+        time = data[:, 0]
+        signal = data[:, 1]
+    else:  # Jeśli jedna kolumna, to to są wartości sygnału EKG, a czas jest indeksem
+        time = np.arange(len(data))
+        signal = data
+
+    print(signal)
+    return time, signal
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def plot_signal(time, signal, start_time, end_time, title="Sygnał EKG", xlabel="Czas (ms)", ylabel="Amplituda",
+                save_path=None):
+    """
+    Funkcja do wizualizacji sygnału EKG.
+    """
+    plt.figure(figsize=(10, 6))
+    plt.plot(time, signal[:, 3], label="Sygnał EKG")
+    plt.xlim(start_time, end_time)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend()
+    plt.grid(True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    if save_path:
+        plt.savefig(save_path)
+    else:
+        plt.show()
+
+
+def main():
+    file_path = input("Podaj ścieżkę do pliku z sygnałem EKG: ")
+    start_time = float(input("Podaj początkowy czas wyświetlanego wycinka: "))
+    end_time = float(input("Podaj końcowy czas wyświetlanego wycinka: "))
+    save_path = input("Podaj ścieżkę do zapisu wycinka (jeśli nie chcesz zapisać, pozostaw puste): ")
+
+    time, signal = load_signal(file_path)
+    plot_signal(time, signal, start_time, end_time, save_path=save_path)
+
+
+if __name__ == "__main__":
+    main()
